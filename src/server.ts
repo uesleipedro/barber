a13ae7abb74e089dd642c1a116af755117a1fc77
uesleipedro <ielseu@hotmail.com>
2022-01-18 22:08:01 -0300

    refactoring express implementation

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,11 @@
-import express, { Request, Response } from 'express';
-import userRoute from './routes/userRoute';
+import { app } from './app';
 
-class Server {
-    private app: express.Application;
+const port = (process.env.PORT || '3000');
 
-    constructor() {
-        this.app = express();
-        this.configuration();
-        this.routes();
-    }
+const server =
+    app.listen(port, () => console.log(`running on port ${port}`));
 
-    public configuration() {
-        this.app.set('port', process.env.PORT || 3000);
-    }
-
-    public routes() {
-        this.app.use('/user', userRoute);
-        this.app.get('/', (req, res) =>{
-            res.send("API ON");
-        });
-    }
-
-    public start() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log(`Server is listening in port ${this.app.get('port')}.`)
-        })
-    }
-}
-
-const server = new Server();
-server.start();
+process.on('SIGINT', () => {
+    server.close();
+    console.log('Connection closed');
+});
