513a23011f9e5afce28dda88f044473a73926866
uesleipedro <ielseu@hotmail.com>
2022-01-23 00:20:46 -0300

    added and configured dotenv lib

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,12 +1,13 @@
 import pgPromise from 'pg-promise';
 const pgp = pgPromise({});
+import 'dotenv/config';
 
 const db = pgp({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.HOST,
     port: 5432,
-    database: 'barber'
+    database: process.env.DATABASE_NAME
 });
 
 export default db;
